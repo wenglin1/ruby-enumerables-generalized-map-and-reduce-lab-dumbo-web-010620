3710418d0264c245c1b737b82dcfdd5a1d564abb
weng lin <wenglin@buffalo.edu>
2020-01-03 17:36:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@ def map(source_array)
   new_arr = []
   i = 0
   while i < source_array.length do
-    new_arr.push(yield(source_array[row]))
-    row += 1
+    new_arr.push(yield(source_array[i]))
+    i += 1
   end
-  new_array
+  new_arr
 end
