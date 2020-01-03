c8cb61260e74b11c6852f2048c518f2af78fc986
weng lin <wenglin@buffalo.edu>
2020-01-03 17:36:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def map(source_array)
+  new_array= []
+  row = 0
+  while row <source_array.length do
+    new_array.push(yield(source_array[row]))
+    row += 1
+  end
+  new_array
+end
