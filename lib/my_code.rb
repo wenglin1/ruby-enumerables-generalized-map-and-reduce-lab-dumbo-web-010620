cbe093d4624c1cd3b52037178b08faeb1a9bf3b9
weng lin <wenglin@buffalo.edu>
2020-01-03 17:38:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,16 @@ def map(source_array)
   end
   new_arr
 end
+
+def reduce(source_array, start = 0)
+  i = 0
+  total = start
+  value = 0
+  while i < source_array.length do
+    value = yield(source_array[i],value)
+    if value == false
+      return false
+    end
+    i += 1
+  end
+end
